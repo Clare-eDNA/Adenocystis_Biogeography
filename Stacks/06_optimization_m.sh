e40efefd8bf2bc807458bcc2e6b101c4f0f154fa
Clare IM Adams <clare.im.adams@gmail.com>
2022-09-22 13:49:00 +1200

    Update and rename 05_optomizationm.sh to 06_optimization_m.sh
    updated file name

--- Stacks/06_optimization_m.sh
+++ Stacks/06_optimization_m.sh
@@ -19,5 +19,5 @@ mkdir -p lil_m$i
 echo '#!/bin/sh' > run_m$i.sh
 echo "module load Stacks/2.61-gimkl-2022a" >> run_m$i.sh
 echo "denovo_map.pl --samples /nesi/nobackup/uoo03666/Adenocystis/trimmed/trimmed/70bp/LargeFiles --popmap samples.txt  -o lil_m$i -p 0.8 -M 3 -n 3 -m $i -T 8 -X \"populations:--vcf --write-random-snp\"" >> run_m$i.sh
-sbatch -A uoo03666 -t 08:00:00 -J m$i -c 8 --mem=64G run_m$i.sh # specific to mahuika and ludovic.dutoit
+sbatch -A uoo03666 -t 08:00:00 -J m$i -c 8 --mem=64G run_m$i.sh # specific to mahuika and [user]
 done
